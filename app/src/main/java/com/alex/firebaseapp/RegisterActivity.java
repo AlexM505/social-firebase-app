99e0c66ba3c3827fb7599cfe4c0c0d42bcf742e8
cmarenco <cmarenco@thedataage.com>
2019-09-06 15:26:57 -0600

    First commit

---
+++ app/src/main/java/com/alex/firebaseapp/RegisterActivity.java
@@ -0,0 +1,104 @@
+package com.alex.firebaseapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText mEmailEt, mPasswordEt;
+    Button mRegisterBtn;
+    ProgressDialog progressDialog;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Crear una cuenta");
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mEmailEt = findViewById(R.id.emailET);
+        mPasswordEt = findViewById(R.id.passwordET);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registrando usuario... ");
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmailEt.getText().toString().trim();
+                String pass = mPasswordEt.getText().toString().trim();
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    mEmailEt.setError("Correo invalido!");
+                    mEmailEt.setFocusable(true);
+                }else if(pass.length() < 6){
+                    mPasswordEt.setError("El tamano de la contrasena debe tener mas de 6 caracteres!");
+                    mPasswordEt.setFocusable(true);
+                }else{
+                    registerUser(email,pass);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String pass) {
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(RegisterActivity.this, "Registrado.. "+user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity.this, ProfileActivity.class));
+                            finish();
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(RegisterActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
