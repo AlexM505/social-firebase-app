6897ab7d2166e172c4734263837205d423caceb6
cmarenco <cmarenco@thedataage.com>
2019-09-11 14:55:46 -0600

    Disenando vista profile y mostrando datos de usuario

--- app/src/main/java/com/alex/firebaseapp/LoginActivity.java
+++ app/src/main/java/com/alex/firebaseapp/LoginActivity.java
@@ -243,19 +243,22 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
 
                             FirebaseUser user = mAuth.getCurrentUser();
-                            String email = user.getEmail();
-                            String uid = user.getUid();
 
-                            HashMap<Object,String> hashMap = new HashMap<>();
-                            hashMap.put("email",email);
-                            hashMap.put("uid",uid);
-                            hashMap.put("name","");
-                            hashMap.put("phone","");
-                            hashMap.put("image","");
+                            if(task.getResult().getAdditionalUserInfo().isNewUser()){
+                                String email = user.getEmail();
+                                String uid = user.getUid();
 
-                            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-                            DatabaseReference reference = firebaseDatabase.getReference("Users");
-                            reference.child(uid).setValue(hashMap);
+                                HashMap<Object,String> hashMap = new HashMap<>();
+                                hashMap.put("email",email);
+                                hashMap.put("uid",uid);
+                                hashMap.put("name","");
+                                hashMap.put("phone","");
+                                hashMap.put("image","");
+
+                                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                                DatabaseReference reference = firebaseDatabase.getReference("Users");
+                                reference.child(uid).setValue(hashMap);
+                            }
 
                             Toast.makeText(LoginActivity.this,""+user.getEmail(),Toast.LENGTH_SHORT).show();
 
